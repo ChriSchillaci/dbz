7cbc22cea42230a4d0f6961c3e0e56f7ef19ed3c
ChriSchillaci <chri2001@outlook.com>
2024-02-05 15:26:59 +0100

    feat: add icons and pagination

--- src/components/buttons/Buttons.jsx
+++ src/components/buttons/Buttons.jsx
@@ -1,18 +1,49 @@
+import {
+  MdOutlineKeyboardArrowLeft,
+  MdOutlineKeyboardArrowRight,
+  MdOutlineKeyboardDoubleArrowLeft,
+  MdOutlineKeyboardDoubleArrowRight,
+} from "react-icons/md";
 import styles from "./index.module.scss";
 
-const Buttons = () => {
+const Buttons = ({ page, setPage }) => {
+  const handlePageBtn = (type) => {
+    if (type === "decrement" && page === 1) setPage(7);
+    else if (type === "increment" && page === 7) setPage(1);
+    else if (type === "decrement" && page >= 2 && 6 <= page)
+      setPage((prev) => --prev);
+    else setPage((prev) => ++prev);
+  };
+
   return (
     <div className={styles.Buttons}>
       <div className={styles["btns-prev"]}>
-        <button className={styles["btn-page"]}>{"<<"}</button>
-        <button className={styles["btn-page"]}>{"<"}</button>
+        <button className={styles["btn-page"]} onClick={() => setPage(1)}>
+          <MdOutlineKeyboardDoubleArrowLeft className={styles["arrow-icons"]} />
+        </button>
+        <button
+          className={styles["btn-page"]}
+          onClick={() => handlePageBtn("decrement")}
+        >
+          <MdOutlineKeyboardArrowLeft className={styles["arrow-icons"]} />
+        </button>
       </div>
       <p>
-        Page <span>1</span> of <span>5</span>
+        Page <span className={styles["number-page"]}>{page}</span> of{" "}
+        <span className={styles["number-page"]}>7</span>
       </p>
       <div className={styles["btns-next"]}>
-        <button className={styles["btn-page"]}>{">"}</button>
-        <button className={styles["btn-page"]}>{">>"}</button>
+        <button
+          className={styles["btn-page"]}
+          onClick={() => handlePageBtn("increment")}
+        >
+          <MdOutlineKeyboardArrowRight className={styles["arrow-icons"]} />
+        </button>
+        <button className={styles["btn-page"]} onClick={() => setPage(7)}>
+          <MdOutlineKeyboardDoubleArrowRight
+            className={styles["arrow-icons"]}
+          />
+        </button>
       </div>
     </div>
   );
