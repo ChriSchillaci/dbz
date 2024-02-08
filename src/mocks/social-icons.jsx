247145171eca253325a7ca73d6a3f5651be3334e
ChriSchillaci <chri2001@outlook.com>
2024-02-08 20:30:54 +0100

    chore: add mockdata

---
+++ src/mocks/social-icons.jsx
@@ -0,0 +1,30 @@
+import {
+  FaFacebook,
+  FaLinkedin,
+  FaGithub,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
+
+export const socialIcons = [
+  {
+    icon: <FaLinkedin />,
+    link: "https://www.linkedin.com/in/christian-schillaci-61ba2b26a/",
+  },
+  {
+    icon: <FaGithub />,
+    link: "https://github.com/ChriSchillaci",
+  },
+  {
+    icon: <FaFacebook />,
+    link: "/",
+  },
+  {
+    icon: <FaTwitter />,
+    link: "/",
+  },
+  {
+    icon: <FaInstagram />,
+    link: "/",
+  },
+];
