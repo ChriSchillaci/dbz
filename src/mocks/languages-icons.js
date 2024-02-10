95fa4387af52716fb646e50eed68971e675e103f
ChriSchillaci <chri2001@outlook.com>
2024-02-10 13:05:57 +0100

    chore: minor changes

--- src/mocks/languages-icons.js
+++ src/mocks/languages-icons.js
@@ -1,98 +1,26 @@
 export const languageIcons = [
-    {
-        link: "https://logos-download.com/wp-content/uploads/2017/07/HTML5_badge.png",
-        alt: 'HTML5'
-    },
-    {
-        link: "https://logospng.org/download/css-3/logo-css-3-2048.png",
-        alt: 'CSS3'
-    },
-    {
-        link: "https://1000logos.net/wp-content/uploads/2020/08/Sass-Logo-2048x1280.png",
-        alt: 'SASS'
-    },
-    {
-        link: "https://logosdownload.com/logo/javascript-logo-big.png",
-        alt: 'JS'
-    },
-    {
-        link: "https://pluspng.com/img-png/react-logo-png-img-react-logo-png-react-js-logo-png-transparent-png-1142x1027.png",
-        alt: 'REACT'
-    },
-    {
-        link: "https://openstandia.jp/oss_info/image/ossinfo_icon_nextjs.png",
-        alt: 'NEXT'
-    },
-    {
-        link: "https://logos-download.com/wp-content/uploads/2017/07/HTML5_badge.png",
-        alt: 'HTML5'
-    },
-    {
-        link: "https://logospng.org/download/css-3/logo-css-3-2048.png",
-        alt: 'CSS3'
-    },
-    {
-        link: "https://1000logos.net/wp-content/uploads/2020/08/Sass-Logo-2048x1280.png",
-        alt: 'SASS'
-    },
-    {
-        link: "https://logosdownload.com/logo/javascript-logo-big.png",
-        alt: 'JS'
-    },
-    {
-        link: "https://pluspng.com/img-png/react-logo-png-img-react-logo-png-react-js-logo-png-transparent-png-1142x1027.png",
-        alt: 'REACT'
-    },
-    {
-        link: "https://openstandia.jp/oss_info/image/ossinfo_icon_nextjs.png",
-        alt: 'NEXT'
-    },
-    {
-        link: "https://logos-download.com/wp-content/uploads/2017/07/HTML5_badge.png",
-        alt: 'HTML5'
-    },
-    {
-        link: "https://logospng.org/download/css-3/logo-css-3-2048.png",
-        alt: 'CSS3'
-    },
-    {
-        link: "https://1000logos.net/wp-content/uploads/2020/08/Sass-Logo-2048x1280.png",
-        alt: 'SASS'
-    },
-    {
-        link: "https://logosdownload.com/logo/javascript-logo-big.png",
-        alt: 'JS'
-    },
-    {
-        link: "https://pluspng.com/img-png/react-logo-png-img-react-logo-png-react-js-logo-png-transparent-png-1142x1027.png",
-        alt: 'REACT'
-    },
-    {
-        link: "https://openstandia.jp/oss_info/image/ossinfo_icon_nextjs.png",
-        alt: 'NEXT'
-    },
-    {
-        link: "https://logos-download.com/wp-content/uploads/2017/07/HTML5_badge.png",
-        alt: 'HTML5'
-    },
-    {
-        link: "https://logospng.org/download/css-3/logo-css-3-2048.png",
-        alt: 'CSS3'
-    },
-    {
-        link: "https://1000logos.net/wp-content/uploads/2020/08/Sass-Logo-2048x1280.png",
-        alt: 'SASS'
-    },
-    {
-        link: "https://logosdownload.com/logo/javascript-logo-big.png",
-        alt: 'JS'
-    },
-    {
-        link: "https://pluspng.com/img-png/react-logo-png-img-react-logo-png-react-js-logo-png-transparent-png-1142x1027.png",
-        alt: 'REACT'
-    },
-    {
-        link: "https://openstandia.jp/oss_info/image/ossinfo_icon_nextjs.png",
-        alt: 'NEXT'
-    },
-]
+  {
+    link: "https://logos-download.com/wp-content/uploads/2017/07/HTML5_badge.png",
+    alt: "HTML5",
+  },
+  {
+    link: "https://logospng.org/download/css-3/logo-css-3-2048.png",
+    alt: "CSS3",
+  },
+  {
+    link: "https://1000logos.net/wp-content/uploads/2020/08/Sass-Logo-2048x1280.png",
+    alt: "SASS",
+  },
+  {
+    link: "https://logosdownload.com/logo/javascript-logo-big.png",
+    alt: "JS",
+  },
+  {
+    link: "https://pluspng.com/img-png/react-logo-png-img-react-logo-png-react-js-logo-png-transparent-png-1142x1027.png",
+    alt: "REACT",
+  },
+  {
+    link: "https://openstandia.jp/oss_info/image/ossinfo_icon_nextjs.png",
+    alt: "NEXT",
+  },
+];
