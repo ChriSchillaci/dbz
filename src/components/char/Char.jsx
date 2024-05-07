5926b356e65fe09018714030d95737b9105e9748
ChriSchillaci <chri2001@outlook.com>
2024-05-07 14:33:01 +0200

    fix: test fix

--- src/components/char/Char.jsx
+++ src/components/char/Char.jsx
@@ -1,12 +1,27 @@
 import Link from "next/link";
+import Image from "next/image";
 import styles from "./index.module.scss";
 
 const Char = ({ char }) => {
   return (
     <li className={styles.Char} id={char.id}>
       <Link href={`character/${char.id}`}>
-        <img className={styles["img-hover"]} src="/images/lines-effect.png" />
-        <img className={styles["Char-img"]} src={char.image} alt={char.name} />
+        <Image
+          className={styles["img-hover"]}
+          src="/images/lines-effect.png"
+          alt="lines"
+          height={1080}
+          width={1920}
+          quality={80}
+        />
+        <Image
+          className={styles["Char-img"]}
+          src={char.image}
+          alt={char.name}
+          height={1080}
+          width={1920}
+          quality={80}
+        />
         <p className={styles.name}>{char.name}</p>
       </Link>
     </li>
